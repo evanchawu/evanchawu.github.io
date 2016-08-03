e339a08fbc360229df35107b678b5110c79bacf5
Kevin <towparadise@gmail.com>
2016-08-03 18:28:52 +0800

    Fix - 把資料庫沒用的表刪除

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,4 @@ ActiveRecord::Schema.define(version: 20160802090856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "names", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email"
-    t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
