203c06e221125fde55f73df13bee3a6d65929092
Kevin <towparadise@gmail.com>
2016-08-22 13:43:54 +0800

    Fix - 修正比賽規章路徑route語法
    路徑不能用中文，改route寫法

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#homepage'
-  get '/草根盃＿比賽規章.pdf', to: '/草根盃＿比賽規章.pdf'
+  get '/contest_policy.pdf', to: redirect('/contest_policy.pdf')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
