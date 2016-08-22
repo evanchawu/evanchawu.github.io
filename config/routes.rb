8ac923535b7e4d23d06cee5c5ace3d1dd03ce23b
Kevin <towparadise@gmail.com>
2016-08-22 15:22:02 +0800

    Feature - create user model
    rails generate devise MODEL
    rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#homepage'
   get '/contest_policy.pdf', to: redirect('/contest_policy.pdf')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
