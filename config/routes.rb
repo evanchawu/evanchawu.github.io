bb240d1756ba25b81063470089226e1773cd877a
Kevin <towparadise@gmail.com>
2016-08-24 15:40:46 +0800

    Feature - 將Sign in 頁面套用獨立的layout
    - 創一個controller，就可以在controller設定到sign_in那頁就會套用到獨立的sign_in layout
    - 要注意要把application.scss 的 *= require_tree . 拿掉，不然其他頁都會讀到stylesheets 下面的sign_in.scss 樣式

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/contest_policy.pdf', to: redirect('/contest_policy.pdf')
 
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', sessions: 'users/sessions' }
 
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
